4d030f8bfe89ed9e81188654993336191ee072d7
Riya Goel <me.riyagoel27@gmail.com>
2021-09-26 15:45:44 +0530

    Add files via upload

---
+++ src/components/Header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.style.css';
+function Header(){
+
+    return (
+<div className="header">
+    <div className="title">recipe box </div>
+    <div className ="btn1"><button>add recipe</button> </div>
+    </div>
+    )
+}
+
+export default Header;
